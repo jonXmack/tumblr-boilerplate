87976071fee167a084d56261b5fc94786fc61c14
Jon MacKinnon <jon@moresoda.co.uk>
2012-01-03 17:31:56 +0000

    Pretty much finished the whole thing, backed up old layout

--- assets/jxm/js/global.js
+++ assets/jxm/js/global.js
@@ -1,6 +1,7 @@
 // iframe resizer - if you're runnning a single column blog
 // From http://blog.kamicrafted.com/post/14312661589/custom-width-for-tumblr-photosets
 
+/*
 $(function(){
 var iFrames = $('.photoset');
 function iResize() {
@@ -26,10 +27,11 @@ function iResize() {
 });
 
 $(".photoset").each(function() { 
-	var newSrc = $(this).attr("src").replace('500','830');
+	var newSrc = $(this).attr("src").replace('500','750');
 	// Replace 830 with your column width
 	$(this).attr("src", newSrc);		
 }); 
+*/
 
 // reblog tool
 
@@ -50,12 +52,69 @@ function GETReblogLink(postId) {
 	str = str.slice(str.lastIndexOf("BEGIN TUMBLR CODE"), str.lastIndexOf("END TUMBLR CODE"));
 	vpid = str.substring(str.search("pid=") + 4, str.search("pid=") + 4 + str.substring(str.search("pid=") + 4).search("&"));
 	vrk = str.substring(str.search("rk=") + 3, str.search("rk=") + 3 + str.substring(str.search("rk=") + 3).search("&"));
-	document.getElementById(postId).href = "http://www.tumblr.com/reblog/" + vpid + "/" + vrk + "?redirect_to=http%3A%2F%2FYOURTUMBLRUSERNAME.tumblr.com%2F";
+	document.getElementById(postId).href = "http://www.tumblr.com/reblog/" + vpid + "/" + vrk + "?redirect_to=http%3A%2F%2Fjonxmack.co.uk%2F";
 }
+
+// Fit text
+
+/*global jQuery */
+/*!	
+* FitText.js 1.0
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+	
+	$.fn.fitText = function( kompressor, options ) {
+	    
+	    var settings = {
+        'minFontSize' : Number.NEGATIVE_INFINITY,
+        'maxFontSize' : Number.POSITIVE_INFINITY
+      };
+	
+			return this.each(function(){
+				var $this = $(this);              // store the object
+				var compressor = kompressor || 1; // set the compressor
+        
+        if ( options ) { 
+          $.extend( settings, options );
+        }
+        
+        // Resizer() resizes items based on the object width divided by the compressor * 10
+				var resizer = function () {
+					$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+				};
+
+				// Call once to set.
+				resizer();
+				
+				// Call on resize. Opera debounces their resize by default. 
+      	$(window).resize(resizer);
+      	
+			});
+
+	};
+
+})( jQuery );
+
 
 $(document).ready(function() {
 
 	// Removes the font-face stylesheet once the google appended one is in to prevent duplication of styles in inspector
 	$('link[title=font-face]').remove();
+
+	$("#logo").fitText(0.343);
+	
+	var navElements 			= $('nav li').length;
+	var	containerWidth			= parseInt(100) - parseInt(navElements);
+	var	finalWidth 				= parseInt(containerWidth) / parseInt(navElements);
+	
+	$('nav li').css('width',finalWidth + '%');
+
 
 });
